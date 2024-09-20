1b18f923a8a56fba43362096f8afe444612ab597
Devendra Kumar Rathore <devendra.rathore@gyansys.com>
2024-09-20 12:54:43 +0530

    changes made in submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './home';
+import Home from './Pages/home';
 
 function App() {
   return (
---
+++ src/Pages/home.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      home Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero error perspiciatis modi,
+      eum aliquam accusantium pariatur id quos nihil saepe aliquid dignissimos nisi vel cupiditate
+      cumque sit dolorum reprehenderit eligendi totam quisquam? Eligendi commodi libero modi quaerat
+      ad cum iure eius exercitationem adipisci, amet molestias voluptas nesciunt excepturi sint
+      natus provident, maiores ex quo vero, recusandae corrupti velit similique soluta! Doloribus,
+      neque voluptatem quisquam cumque optio eveniet harum commodi facere, accusantium placeat aut
+      itaque aspernatur ad deleniti quis odit eos nihil. Dignissimos aspernatur aperiam doloremque
+      nihil velit aliquid nam dolorum odio natus perspiciatis soluta iure ipsa nisi et veritatis
+      molestiae, fugit esse asperiores expedita culpa itaque dolor accusamus nostrum amet? Quasi
+      dolore quibusdam quidem, iure nostrum illo illum quaerat consequuntur ratione sequi magnam in
+      eveniet, non, sint tempore assumenda neque unde! Ex, esse. Ad illo placeat veritatis
+      consequatur nesciunt repellendus iste quo rem maxime quam quas, ut laborum nostrum possimus
+      delectus quos repudiandae reiciendis magnam odit ipsam aliquam harum aliquid odio iure?
+      Libero, dignissimos, omnis cum rerum debitis sunt ut culpa iusto cupiditate minima esse fugiat
+      repellendus ratione nemo quam dicta molestias reiciendis inventore, totam harum ipsa porro.
+      Esse, suscipit facilis! Quidem nihil architecto totam repudiandae sit quaerat, autem non
+      quisquam delectus tempore at! Neque debitis ut porro minus cumque, vero nostrum fuga maiores
+      magnam, vel tempora explicabo officiis vitae pariatur. Saepe debitis earum molestias, nam
+      assumenda facere minus consectetur in quas quisquam ipsa repudiandae quam vero reprehenderit
+      neque suscipit totam molestiae officiis? Asperiores consectetur accusantium illum non beatae
+      porro. Necessitatibus perferendis facilis, tenetur fuga alias omnis consequuntur veritatis quo
+      at eaque corporis dolorem itaque quam error? Temporibus alias, ducimus in quidem iusto earum
+      amet magnam eveniet et recusandae officia perspiciatis dignissimos quam dolores atque cum aut
+      hic fugiat sed reiciendis porro distinctio adipisci sunt quia. Dignissimos laborum unde,
+      facilis esse recusandae repudiandae quo libero dolore cum quibusdam maxime praesentium
+      laudantium quae quia ea illo doloribus saepe quos sit doloremque. Dignissimos velit
+      repellendus architecto quod voluptatibus inventore cum accusantium quis eos possimus nisi,
+      odio, ducimus harum? Eveniet assumenda aspernatur veniam vitae nisi dolorum distinctio
+      maiores! Itaque nulla fugit dolorem sunt voluptate, at, quod expedita laudantium obcaecati aut
+      quasi accusantium, nesciunt fuga quibusdam illo. In numquam praesentium ea. Nesciunt quaerat
+      error asperiores reprehenderit recusandae fugiat labore, quia nemo laudantium doloremque,
+      architecto facere quam expedita neque minus numquam sapiente dolor dicta sed nam odit,
+      repudiandae optio reiciendis. Doloribus in sint reprehenderit alias.
+    </div>
+  );
+};
+
+export default Home;
