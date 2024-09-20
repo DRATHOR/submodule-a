2b80772d49d6cd13261168c0bca1878ecccc8138
Devendra Kumar Rathore <devendra.rathore@gyansys.com>
2024-09-20 12:49:23 +0530

    change in home page

--- src/home.tsx
+++ src/home.tsx
@@ -7,7 +7,9 @@ const Home = () => {
     },[]);
   return (
     <div>
-      home
+      home Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Praesentium voluptate, earum at eaque amet tenetur quibusdam dolore nemo distinctio nam 
+       inventore sunt veniam possimus sint ad. Deserunt, sequi! Earum quod deleniti omnis explicabo suscipit et placeat maiores ex sed, perspiciatis vel dicta labore dolore magni deserunt ducimus commodi. Hic commodi modi, voluptatem vitae quas aliquid dolorum exercitationem ea deleniti odio, explicabo est pariatur nobis dolore blanditiis ipsam eveniet consequatur cumque voluptate quidem perferendis nemo harum maxime. In odit tempora sunt officia eveniet quae aliquid enim repudiandae saepe suscipit voluptas aut, rem delectus expedita eius porro eligendi ullam recusandae? Animi in atque commodi eum architecto. Nam, possimus deserunt? Asperiores delectus illum temporibus id. Maxime, vel dolorem possimus autem, voluptatem aliquid totam libero inventore quod eaque doloribus. Pariatur autem consequuntur ut ullam! Distinctio, sequi quisquam asperiores temporibus numquam ad obcaecati officia quasi! Cupiditate voluptatem dolore quia fugiat est optio tenetur qui sit ab enim in, aliquid inventore incidunt delectus nam temporibus cum exercitationem voluptatum obcaecati facere placeat. Vel dolor id neque explicabo similique corporis deleniti, dolorem perspiciatis, nam mollitia corrupti tempora delectus, alias vitae hic excepturi optio praesentium. Sed ducimus non, aperiam laborum eaque, incidunt adipisci nemo voluptatum unde quasi dolor iusto?
     </div>
   )
 };
